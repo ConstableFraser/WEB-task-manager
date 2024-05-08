acd2d5c64618e2c4faafac766e1bbd5a9a76b151
Alexander Shvedchikov <105097978+ConstableFraser@users.noreply.github.com>
2024-05-08 22:09:08 +0300

    Update AppApplication.java

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class, 
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 @RestController
 public class AppApplication {
     public static void main(String[] args) {
